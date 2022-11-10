b23bea4014399984e78e83102f845cffec9c4b03
damanM <daman.m@outlook.com>
2022-11-09 21:37:24 -0500

    testing

--- appApi.js
+++ appApi.js
@@ -1,3 +1,10 @@
+
+// on page load  -  have static img and placeholder text with dropodwn menu above it
+// addeventlistener to dropdown and update page according to user choice by replacing the static img and placeholder text with api data
+// the fetch call will be exceuted within the addeventlistener 
+// drop down nemu will have 5 character options with corressonding id value
+// once user selects character - page updates with img, name, status, species and gender of chosen character
+
 const ramApp = {};
 
 
@@ -7,11 +14,14 @@ ramApp.getCharacter = () => {
     
     fetch(url)
     .then( (res) => {
-        console.log('yes')
+        return res.json();
     })
- 
+    .then ((data) =>{
+        console.log(data)
+    })
 }
 
+console.log('featuretest')
 
 ramApp.init = () => {
     console.log('yea');
